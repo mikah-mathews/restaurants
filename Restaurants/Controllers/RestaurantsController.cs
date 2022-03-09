e242d404b96bda7aa7e8c07ed9353859ecb910c1
Will Greenberg <greenberg.will@gmail.com>
2022-03-09 14:14:20 -0800

    add views and controller methods
    Co-Authored-By: Mikah Mathews <mikah@outofband.org>

---
+++ Restaurants/Controllers/RestaurantsController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Restaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Restaurants.Controllers
+{
+  public class RestaurantsController : Controller
+  {
+    private readonly RestaurantsContext _db;
+    public RestaurantsController(RestaurantsContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Restaurant> model = _db.Restaurants.ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+      
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Restaurant restaurant)
+    {
+      _db.Restaurants.Add(restaurant);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Details(int id)
+    {
+      Restaurant thisRestaurant = _db.Restaurants.FirstOrDefault(restaurant => restaurant.RestaurantId == id);
+      return View(thisRestaurant);
+    }
+    public ActionResult Edit(int id)
+    {
+      Restaurant thisRestaurant = _db.Restaurants.FirstOrDefault(restaurant => restaurant.RestaurantId == id);
+      return View(thisRestaurant);
+    }
+    [HttpPost]
+    public ActionResult Edit(Restaurant restaurant)
+    {
+      _db.Entry(restaurant).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
